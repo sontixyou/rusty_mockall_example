ad0bf5ee7e62deaaa08fa5f7e85c89fded50bb7a
sontixyou <kengoviolinphoto@gmail.com>
2024-10-03 12:24:21 +0900

    add mokito mock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//
+// mockallを使ったモックのサンプル
+//
 use mockall::predicate::*;
 use mockall::*;
 
@@ -23,6 +26,55 @@ fn test_database_query_calls() {
 
     // これは任意。これがなくても３回未満で呼ばれた場合テストが落ちる
     mock.checkpoint();
+}
+
+//
+// mockitoを使ったHTTPリクエストのモック
+//
+use reqwest::Response;
+use serde_json::json;
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct ApiResponseBody {
+    timestamp: String,
+    holiday_name: String,
+}
+
+pub async fn fetch_holidays_jp_in_year() -> Result<Response, reqwest::Error> {
+    let url = "https://holidays-jp.github.io/api/v1/2021/datetime.json";
+    let response: Response = reqwest::get(url).await?;
+    Ok(response)
+}
+
+#[tokio::test]
+async fn test_fetch_holidays_jp_in_year() {
+    // doc: https://docs.rs/mockito/latest/mockito/struct.ServerOpts.html
+    let opts = mockito::ServerOpts {
+        // NOTE: IPアドレスを指定するのみ
+        // host: "https://holidays-jp.github.io",
+        host: "0.0.0.0",
+        ..Default::default()
+    };
+    let mut server_with_host = mockito::Server::new_with_opts(opts);
+
+    let response_body = ApiResponseBody {
+        timestamp: "2021-01-01".to_string(),
+        holiday_name: "元日".to_string(),
+    };
+
+    let _m = server_with_host
+        .mock("GET", "/api/v1/2021/datetime.json")
+        .with_status(200)
+        .with_header(
+            reqwest::header::CONTENT_TYPE.to_string(),
+            "application/json",
+        )
+        .with_body(json!(response_body).to_string());
+
+    // モックサーバーにリクエストを送信
+    let response = fetch_holidays_jp_in_year().await.unwrap();
+
+    println!("response: {:?}", response);
 }
 
 fn main() {
