d8a04bb10f96957c257c82f72162dd314c552241
sontixyou <kengoviolinphoto@gmail.com>
2024-10-08 11:06:28 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,4 @@
 //
-// mockallを使ったモックのサンプル
-//
-use mockall::predicate::*;
-use mockall::*;
-
-// モックしたい構造体やトレイトの定義
-#[automock]
-trait Database {
-    fn query(&self, sql: &str) -> Vec<String>;
-}
-
-#[test]
-fn test_database_query_calls() {
-    // モックオブジェクトの作成
-    let mut mock = MockDatabase::new();
-
-    mock.expect_query()
-        .with(eq("SELECT * FROM users"))
-        .times(3) // この関数が3回呼ばれることを期待
-        .returning(|_| vec!["user1".to_string(), "user2".to_string()]);
-
-    for _ in 0..3 {
-        mock.query("SELECT * FROM users");
-    }
-
-    // これは任意。これがなくても３回未満で呼ばれた場合テストが落ちる
-    mock.checkpoint();
-}
-
-//
 // mockitoを使ったHTTPリクエストのモック
 //
 
@@ -53,7 +23,8 @@ static PATH: &str = "http://0.0.0.0:1234/todos/";
 async fn main() {
     let todo_id = 1;
     // NOTE:
-    // 変数urlの値を変更したときは、テストがコケるべきときにコケてくれない。これを守るためにRustではどう書けば良いのか?
+    // 変数urlの値を変更したときは、テストがコケるべきときにコケてくれない。
+    // これを守るためにRustではどう書けば良いのか?
     // 毎回モックサーバーを起動するのが手間だけど、Rustでは基本的な方法？
     let url = "https://jsonplaceholder.typicode.com/todos/";
     match run(url, todo_id).await {
